72c8dd62aec8c6cee66fe326af33fbedd652996d
Nikita Kolpakovsky <nikitaunity@gmail.com>
2024-02-08 23:10:53 +0300

    08.02.2024

---
+++ script.js
@@ -0,0 +1,69 @@
+'use strict'
+
+// alert("hello")
+// const answer = prompt("ваш возраст?", 4)
+// const answer2 = confirm('Ваш возраст больше -1?')
+// console.log(answer, answer2);
+let string1 = 'hello'
+let string2 = "he''llo"
+let string3 = `he"''"l'""'lo`
+let isTrue = true // false
+let int = 222222222222222222222222222222222222222222222n // bigint
+var b = 2 // "2"
+const MAX_VALUE_OF_NUMBERS = 1
+// a = 3
+let object = {
+    imya: "nikita",
+    voszrast: 20,
+    korobka: 'big',
+}
+let variable // == let variable = undefined
+let whole = null 
+let korzina
+let sumb = Symbol('hello im symbol')
+let number = NaN
+let count = 10_000_000_000_000_000
+let num = 1_000
+// alert(20)
+b = object.voszrast
+// zxcOadadasOppo //    camelCase correct
+// zxc_oadads_oppo //   snake_case
+// zxc-oadadas-oppo  // kebab-case
+// ZxcOadadasOppo //    PascalCase
+let azxc = {name: 'kirill'}
+let isOpenUserWindowOfRegistaration = false
+let array = [{name: 'kirill'}, {name: 'oleg'}, {name: 'masha'}, ] // 0 , 1 ,2
+// console.log(string1, 'sadad', int, array, object);
+// console.log( typeof number, b , b.toString(), String(b), parseFloat("22"));
+// console.log(count, Boolean(count), !!count, !!isTrue, Number("20"), `` + b);
+// console.log(((20-2-2+5)/2*4)**2 % 8);
+// console.log(parseInt(5/3, 10), 5/3, 5 % 3, 'asd' + 'qwe', true + false, !!{} + true + !'');
+let var2 = 4 + 5  + "asdasd"
+var2 = string1
+// console.log(array[0], array.length);
+// let numberBig = object.korobka
+// console.log(string1, string1.length, string1[3]);
+
+// console.log(array[0].name[5], array[1].name[1], array[0].name[5] == array[1].name[1]);
+
+let arrayOfStrings = ['hello', 'mello', 'zello']
+console.log(arrayOfStrings[1][2]);
+let arrayOfArrays = [
+    
+    [['abc', 'rtrd'],['defg'],['ikl']],
+    [['trew'],['qdsa', 'zxcaaa'],['asdqw']],
+    [['bron'],['lkbn'],[b, 'erewr']],
+]
+// console.log(arrayOfArrays[0][0][1][0] == arrayOfArrays[1][0][0][1]); // r r ...etc
+// console.log(2 == '2');
+// console.log(1 === 3);
+// console.log( 2 !== '2');
+const MEGA_STRING = `hello my name console.log ${prompt("ваш возраст?", 4)} is: ${string1}, age: ${b}, my favourite word is ${arrayOfArrays[0][2][0]}`
+console.log(MEGA_STRING);
+
+
+
+
+
+
+
